3307e1fe069a8d7fbeee859856962f8745965b77
c0643680 <c0643680@SB-N208-10.lambton.on.ca>
2015-01-14 15:13:46 -0500

    firstcommit

--- src/ordercontrol/OrderControl.java
+++ src/ordercontrol/OrderControl.java
@@ -17,6 +17,7 @@ public class OrderControl {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hello");
     }
     
 }
